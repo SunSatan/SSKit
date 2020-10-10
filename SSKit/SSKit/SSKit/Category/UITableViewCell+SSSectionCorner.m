480f7c4eaf21a03d3d51dc9a008cd2ad4640c152
SunSatan <807160928@qq.com>
2020-10-10 18:40:43 +0800

    add
    add

--- SSKit/SSKit/SSKit/Category/UITableViewCell+SSSectionCorner.m
+++ SSKit/SSKit/SSKit/Category/UITableViewCell+SSSectionCorner.m
@@ -10,8 +10,9 @@
 
 @interface UITableViewCell ()
 
-@property (nonatomic, strong) CAShapeLayer *topLay;
-@property (nonatomic, strong) CAShapeLayer *bottomLay;
+@property (nonatomic, strong) CAShapeLayer *allCorners;
+@property (nonatomic, strong) CAShapeLayer *topCorners;
+@property (nonatomic, strong) CAShapeLayer *bottomCorners;
 
 @end
 
@@ -21,60 +22,78 @@
                                indexPath:(NSIndexPath *)indexPath
                             cornerRadius:(CGFloat)cornerRadius
 {
+    //创建圆角
+    if (!self.allCorners) {
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = self.contentView.bounds;
+        maskLayer.path = maskPath.CGPath;
+        self.allCorners = maskLayer;
+    }
+    if (!self.topCorners) {
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerTopLeft|UIRectCornerTopRight  cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = self.contentView.bounds;
+        maskLayer.path = maskPath.CGPath;
+        self.topCorners = maskLayer;
+    }
+    if (!self.bottomCorners) {
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerBottomLeft | UIRectCornerBottomRight  cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = self.contentView.bounds;
+        maskLayer.path = maskPath.CGPath;
+        self.bottomCorners = maskLayer;
+    }
+    
+    
     if ([tableView numberOfRowsInSection:indexPath.section] == 1) {
-        if (self.topLay == nil) {
-            UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
-            CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-            maskLayer.frame = self.contentView.bounds;
-            maskLayer.path = maskPath.CGPath;
-            self.topLay = maskLayer;
-        }
-        self.contentView.layer.mask = self.topLay;
+        //只有一个 row 则全圆角
+        self.contentView.layer.mask = self.allCorners;
     }
     else if (indexPath.row == 0) {
-        if (self.topLay == nil) {
-            UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight  cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
-            CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-            maskLayer.frame = self.contentView.bounds;
-            maskLayer.path = maskPath.CGPath;
-            self.topLay = maskLayer;
-        }
-        self.contentView.layer.mask = self.topLay;
+        // 开始 row 则顶部圆角
+        self.contentView.layer.mask = self.topCorners;
     }
     else if (indexPath.row == [tableView numberOfRowsInSection:indexPath.section] - 1){
-        if (self.bottomLay == nil) {
-            UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.contentView.bounds byRoundingCorners:UIRectCornerBottomLeft | UIRectCornerBottomRight  cornerRadii:CGSizeMake(cornerRadius, cornerRadius)];
-            CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-            maskLayer.frame = self.contentView.bounds;
-            maskLayer.path = maskPath.CGPath;
-            self.bottomLay = maskLayer;
-        }
-       self.contentView.layer.mask = self.bottomLay;
+        // 结束 row 则顶部圆角
+       self.contentView.layer.mask = self.bottomCorners;
     }
     else {
+        // 中间 row 则没有圆角
         self.contentView.layer.mask = nil;
     }
 }
 
-- (void)setTopLay:(CAShapeLayer *)topLay
+#pragma mark - setter && getter
+
+- (void)setAllCorners:(CAShapeLayer *)allCorners
 {
-    objc_setAssociatedObject(self, @selector(topLay), topLay, OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, @selector(allCorners), allCorners, OBJC_ASSOCIATION_RETAIN);
 }
 
-- (CAShapeLayer *)topLay
+- (CAShapeLayer *)allCorners
+{
+    return objc_getAssociatedObject(self, _cmd);
+}
+
+- (void)setTopCorners:(CAShapeLayer *)topCorners
+{
+    objc_setAssociatedObject(self, @selector(topCorners), topCorners, OBJC_ASSOCIATION_RETAIN);
+}
+
+- (CAShapeLayer *)topCorners
 {
     return objc_getAssociatedObject(self, _cmd);
 }
  
-- (void)setBottomLay:(CAShapeLayer *)bottomLay
+- (void)setBottomCorners:(CAShapeLayer *)bottomCorners
 {
-    objc_setAssociatedObject(self, @selector(bottomLay), bottomLay, OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, @selector(bottomCorners), bottomCorners, OBJC_ASSOCIATION_RETAIN);
 }
 
-- (CAShapeLayer *)bottomLay
+- (CAShapeLayer *)bottomCorners
 {
     return objc_getAssociatedObject(self, _cmd);
 }
-
 
 @end
